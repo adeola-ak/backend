0efa67633fec87d2d724c1b568c109da0ee161d6
adeola <adeola@Adeolas-MBP.fios-router.home>
2020-10-26 12:38:43 -0400

    more troubleshooting

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017/palate";
+const mongoURI = process.env.mongoURI;
 
 const config = {
 	useUnifiedTopology: true,
