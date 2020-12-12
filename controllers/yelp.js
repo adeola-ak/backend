cb9ec8c882a6a93cc3c0af7bf3cb7cac635bdba4
Leah Haake <leahhaake@Leahs-MacBook-Air.local>
2020-12-12 10:01:26 -0500

    edits to Yelp controller. Yelp data is being console logged

--- controllers/yelp.js
+++ controllers/yelp.js
@@ -1,8 +1,35 @@
-const { Router, response } = require("express");
+const {Router} = require("express");
 const router = Router();
-const API_KEY = process.env.apikey;
-const axios = require("axios");
+const API_KEY = process.env.API_KEY;
+const fetch = require('node-fetch');
+// const axios = require("axios");
 
+
+// need to install fetch for node "npm install node-fetch" and require in file const fetch = require('node-fetch');
+// route parameters to endpoint "/data/:zip/:rest"
+	// const zip = req.params.zip
+	//const rest = req.params.rest
+router.get('/data/:zip/:rest', async (req, res) => {
+	const zip = req.params.zip
+	const rest = req.params.rest
+	const api_url = `https://api.yelp.com/v3/businesses/search?location=${zip}&term=${rest}`
+	const fetch_response = await fetch(api_url, {
+		method: "GET",
+		headers: {
+		Authorization: `Bearer ${API_KEY}`,
+	}
+	})
+	const json = await fetch_response.json()
+	res.json(json)
+})
+
+
+
+module.exports = router;
+
+
+
+// Code we tried earlier
 // router.post("/data", async (req, res) => {
 // 	const rest = req.body.restaurant;
 // 	const zip = req.body.zipcode;
@@ -28,7 +55,3 @@ const axios = require("axios");
 	
 
 // });
-
-
-
-module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
-const axios = require("axios");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
-const fetch = require('node-fetch');
+// const axios = require("axios");
+// const fetch = require('node-fetch');
 
 const PORT = process.env.PORT;
 const API_KEY = process.env.API_KEY;
@@ -13,10 +13,10 @@ const itemRouter = require("./controllers/item");
 const ratingRouter = require("./controllers/rating");
 const yelpRouter = require("./controllers/yelp");
 
+
 app.get("/", (req, res) => {
 	res.send("well, hello there world..");
 });
-
 
 
 
@@ -67,26 +67,6 @@ app.use("/items", itemRouter);
 app.use("/ratings", ratingRouter);
 app.use("/yelp", yelpRouter);
 
-
-// need to install fetch for node "npm install node-fetch"
-// route parameters to endpoint "/data/:zip/:rest"
-	// const zip = req.params.zip
-	//const rest = req.params.rest
-// Auth in fetch 
-
-app.get('/data/:zip/:rest', async (req, res) => {
-	const zip = req.params.zip
-	const rest = req.params.rest
-	const api_url = `https://api.yelp.com/v3/businesses/search?location=${zip}&term=${rest}`
-	const fetch_response = await fetch(api_url, {
-		method: "GET",
-		headers: {
-		Authorization: `Bearer ${API_KEY}`,
-	}
-	})
-	const json = await fetch_response.json()
-	res.json(json)
-})
 
 app.listen(PORT, () => {
 	console.log(`listening in on port: ${PORT}`);
