dd78e25ed051a8cd22f269b4f361c39bc635453a
Leah Haake <leahhaake@Leahs-MacBook-Air.local>
2020-12-11 23:54:35 -0500

    yelp api call server side tested

--- controllers/yelp.js
+++ controllers/yelp.js
@@ -1,23 +1,34 @@
-const { Router } = require("express");
+const { Router, response } = require("express");
 const router = Router();
 const API_KEY = process.env.apikey;
 const axios = require("axios");
 
-router.post("/data", async (req, res) => {
-	const rest = req.body.restaurant;
-	const zip = req.body.zipcode;
-	await axios({
-		url: `https://api.yelp.com/v3/businesses/search?location=${zip}&term=${rest}`,
-		method: "GET",
-		headers: {
-			Authorization: `Bearer ${API_KEY}`,
-		},
-	}).then((response) => {
-		res.json({
-			status: 200,
-			yelp: response.data,
-		});
-	});
-});
+// router.post("/data", async (req, res) => {
+// 	const rest = req.body.restaurant;
+// 	const zip = req.body.zipcode;
+// 	try {
+// 	await axios({
+// 		url: `https://api.yelp.com/v3/businesses/search?location=${zip}&term=${rest}`,
+// 		method: "GET",
+// 		headers: {
+// 			Authorization: `Bearer ${API_KEY}`,
+// 		},
+// 	})
+// 	.then((response) => {
+// 		res.json({
+// 			status: 200,
+// 			yelp: response.data,
+// 		});
+// 	})
+// 	} catch(err) {
+// 		res.json({
+// 			status: 404
+// 		})
+// 	}
+	
+
+// });
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
+const fetch = require('node-fetch');
+
 const PORT = process.env.PORT;
-const API_KEY = process.env.apikey;
+const API_KEY = process.env.API_KEY;
 const restaurantRouter = require("./controllers/restaurant");
 const itemRouter = require("./controllers/item");
 const ratingRouter = require("./controllers/rating");
@@ -14,6 +16,9 @@ const yelpRouter = require("./controllers/yelp");
 app.get("/", (req, res) => {
 	res.send("well, hello there world..");
 });
+
+
+
 
 //This is for search..display 5 local resturants on the home page
 
@@ -61,6 +66,27 @@ app.use("/restaurants", restaurantRouter);
 app.use("/items", itemRouter);
 app.use("/ratings", ratingRouter);
 app.use("/yelp", yelpRouter);
+
+
+// need to install fetch for node "npm install node-fetch"
+// route parameters to endpoint "/data/:zip/:rest"
+	// const zip = req.params.zip
+	//const rest = req.params.rest
+// Auth in fetch 
+
+app.get('/data/:zip/:rest', async (req, res) => {
+	const zip = req.params.zip
+	const rest = req.params.rest
+	const api_url = `https://api.yelp.com/v3/businesses/search?location=${zip}&term=${rest}`
+	const fetch_response = await fetch(api_url, {
+		method: "GET",
+		headers: {
+		Authorization: `Bearer ${API_KEY}`,
+	}
+	})
+	const json = await fetch_response.json()
+	res.json(json)
+})
 
 app.listen(PORT, () => {
 	console.log(`listening in on port: ${PORT}`);
